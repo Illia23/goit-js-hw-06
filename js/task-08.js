5f446b12a92d1a4390d20bf14867d334807f1a7e
Illia23 <Priymak2331@gmail.com>
2023-09-12 17:26:54 -0500

    task-8:Done)

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", formValue);
+
+function formValue(elem) {
+    elem.preventDefault();
+    const { email, password } = elem.currentTarget.elements;
+
+    if (!email.value.trim() || !password.value.trim()) {
+        alert("Всі поля повині бути заповнені");
+
+    } else {
+        const formData = {
+            email: email.value,
+            password: password.value,
+
+        };
+        console.log(formData);
+        elem.currentTarget.reset();
+    }
+}
